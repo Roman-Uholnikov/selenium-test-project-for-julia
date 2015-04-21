e433036eb0cb9f2ecb5a0fb36920a5978d2906ad
Roman Ugolnikov <r.uholnikov@levi9.com>
2015-04-21 13:34:44 +0300

    dou template

---
+++ src/test/java/automation/DouTest.java
@@ -0,0 +1,26 @@
+package automation;
+
+import org.junit.Test;
+
+/**
+ * @author Roman Uholnikov
+ */
+public class DouTest {
+
+    /**
+     * This method should search through all companies in dou and get their information (names and emails).
+     * @throws Exception
+     */
+    @Test
+    public void getCompaniesNamesAndEmails() throws Exception {
+        //open companies list on the dou
+        //for each page
+            //go to the each company
+                //go to tap offices and get info for particular town.
+    }
+
+    private void sendEmails(String[] emails, String title, String TextMessage, String attachetFileName){
+        //create email
+        //send email to the each company and show it in the log
+    }
+}
