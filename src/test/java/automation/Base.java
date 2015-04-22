0a22481c9073a8284133fb4b17f4281293883fee
Roman Ugolnikov <r.uholnikov@levi9.com>
2015-04-22 17:20:11 +0300

    complete version

--- src/test/java/automation/Base.java
+++ src/test/java/automation/Base.java
@@ -4,10 +4,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by roman on 07.02.15.
@@ -15,11 +18,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Base {
 
     protected WebDriver driver = new FirefoxDriver();
+    protected WebDriver driverForEmail = new FirefoxDriver();
 
     public WebDriver getDriver() {
         return driver;
     }
 
+
+    public WebDriver getDriverForEmail() {
+        return driverForEmail;
+    }
     public void setDriver(WebDriver driver) {
         this.driver = driver;
     }
@@ -32,11 +40,20 @@ public class Base {
 
         // Make sure all elements are visible.
         window.maximize();
+
+        this.getDriverForEmail();
+        WebDriver.Options optionsEmail = getDriverForEmail().manage();
+        WebDriver.Window windowEmail = optionsEmail.window();
+
+        // Make sure all elements are visible.
+        windowEmail.maximize();
+
     }
 
     @After
     public void tearDown() {
         getDriver().quit();
+        getDriverForEmail().quit();
     }
 
 //    /**
@@ -81,5 +98,35 @@ public class Base {
         // Make sure all elements are visible.
         window.maximize();
         return driver;
+    }
+
+    /**
+     * Insert text to iframe input
+     *
+     * @param frameXpath field name
+     * @param text      text that going to be entered
+     * @throws Exception
+     */
+    public void enterTextToIFrame(String frameXpath, String text, WebDriver driver) {
+
+        //enter text to iFrame
+        pause(2);
+        WebElement idElement = driver.findElement(By.xpath(frameXpath));
+        driver.findElement(By.xpath(frameXpath)).click();
+        pause(2);
+        driver.switchTo().frame(idElement);
+        WebElement body = driver.findElement(By.tagName("body"));
+        body.sendKeys(Keys.CONTROL + "a");
+        body.sendKeys(text);
+        body.sendKeys(Keys.ENTER);
+        driver.switchTo().defaultContent();
+    }
+
+    /**
+     * @param seconds amount of seconds
+     * @param locator elements which is expected
+     */
+    public void pause(String locator, long seconds) {
+        new WebDriverWait(driver, seconds).until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
     }
 }
--- src/test/java/automation/DouTest.java
+++ src/test/java/automation/DouTest.java
@@ -21,10 +21,12 @@ public class DouTest extends Base {
     public static final String ATTACHET_FILE_NAME = "Julia Loboda.docx";
     public static final String MESSAGE_TEXT = "Good day! \n" +
             "\n" +
-            "My name Is Julia, I am QA Engineer; I am looking for job, that will give me a chance to be the best specialist in my sphere. I want to improve my skills and be useful for team in your company! I am interested in new experience, in the excellent result, that is why I can be interesting for you! Thank you in advance for your attention!\n" +
+            "My name Is Julia, I am QA Engineer; I am looking for new job, that will give me a chance to be the best specialist in my sphere. I want to improve my skills and be useful for team in your company! I am interested in new experience, in the excellent result, that is why I can be interesting for you! Thank you in advance for your attention!\n" +
             "\n\n" +
             "With kind regards,\n" +
             "Julia Loboda";
+    private static final String USER_NAME = "111";
+    private static final String USER_PASSWORD = "111";
 
     /**
      * This method should search through all companies in dou and get their information (names and emails).
@@ -37,11 +39,11 @@ public class DouTest extends Base {
         getDriver().get(DOU_UA_COMPANIES);
         /* unwrap as much as possible pages*/
         for (int i = 0; i < HOW_MUCH_PAGE_DIAL_WITH; i++) {
-            Thread.sleep(1000);
-            if(getDriver().findElements(By.xpath("//div[@class='more-btn']/a")).size()>0){
+            pause("//div[@class='more-btn']/a", 5);
+            if (getDriver().findElements(By.xpath("//div[@class='more-btn']/a")).size() > 0) {
                 scrollDownToElement(By.xpath("//div[@class='more-btn']/a"));
                 getDriver().findElement(By.xpath("//div[@class='more-btn']/a")).click();
-            }else{
+            } else {
                 break;
             }
         }
@@ -51,20 +53,52 @@ public class DouTest extends Base {
         pause(1);
         List<WebElement> companiesWebElements = getDriver().findElements(By.xpath("//div[@class='company']"));
 
+//        for(int i=0; i<3; i++){ WebElement companyWebElement = companiesWebElements.get(i); debuging version
         for (WebElement companyWebElement : companiesWebElements) {
             DouCompany company = getDouCompany(companyWebElement);
-            listOfCompanies.add(company);
-            System.out.println(listOfCompanies.size() + " " + company.toString());
-            //sendEmails();
-            for(String email: company.getEmails()){
-                sendEmails(email, EMAIL_SUBJECT, MESSAGE_TEXT , ATTACHET_FILE_NAME);
+            if(company.getEmails().size() > 0){
+                listOfCompanies.add(company);
+                System.out.println(listOfCompanies.size() + " " + company.toString());
+            }
+        }
+
+        /* sending emails */
+        for(DouCompany company: listOfCompanies){
+            for(String email:company.getEmails()){
+                sendEmails(email, EMAIL_SUBJECT, MESSAGE_TEXT, ATTACHET_FILE_NAME);
             }
         }
     }
 
     private void sendEmails(String emailReveiver, String title, String TextMessage, String attachetFileName) {
-        //create email
-        //send email to the each company and show it in the log
+        String url = "https://e.mail.ru/compose/";
+        WebDriver webDriverforEmail = getDriverForEmail();
+        webDriverforEmail.get(url);
+
+        if (webDriverforEmail.findElements(By.name("Login")).size() > 1) {
+            webDriverforEmail.findElement(By.name("Login")).sendKeys(USER_NAME);
+            webDriverforEmail.findElement(By.name("Password")).sendKeys(USER_PASSWORD);
+            //click send
+            webDriverforEmail.findElement(By.xpath("//form[@id='LoginExternal']//div[@class='popup__controls']/button[@type='submit']/span")).click();
+            pause(5);
+        }
+
+        webDriverforEmail.findElement(By.xpath(".//*[@id='compose__header__content']/div[2]/div[2]/div[1]/textarea")).sendKeys(emailReveiver);
+
+        webDriverforEmail.findElement(By.xpath("//input[@name='Subject']")).sendKeys(title);
+
+        webDriverforEmail.findElement(By.xpath("//div[@title='Файлы из Облака и Почты']")).click();
+        pause(2);
+        webDriverforEmail.findElement(By.xpath("//div[contains(.,'" + attachetFileName + "')]/label")).click();
+        webDriverforEmail.findElement(By.xpath("//div[@class='popup__controls']/button[@type='submit']/span[contains(.,'Прикрепить')]")).click();
+
+
+//        webDriverforEmail.findElement(By.id("compose_276_composeEditor_ifr")).sendKeys(MESSAGE_TEXT);
+        enterTextToIFrame("//tr[@class='mceFirst mceLast']//iframe", MESSAGE_TEXT, getDriverForEmail());
+
+        webDriverforEmail.findElement(By.xpath("(//div[@data-name='send'])[1]")).click();
+        System.out.println("email have been sent to: " + emailReveiver);
+        pause(2);
     }
 
     private DouCompany getDouCompany(WebElement webElement) throws InterruptedException {
@@ -81,7 +115,7 @@ public class DouTest extends Base {
             driver1.get(link);
 
 
-            Thread.sleep(1000);
+            pause(1);
             List<WebElement> addressesWebElementList = driver1.findElements(By.xpath("//div[@class='table' and preceding-sibling::h4[contains(.,'Ки')]]//div[@class='contacts']/div[@class='address']"));
             List<WebElement> mailWebElementList = driver1.findElements(By.xpath("//div[@class='table' and preceding-sibling::h4[contains(.,'Ки')]]//div[@class='contacts']/div[@class='mail']/a"));
             List<WebElement> phonesWebElementList = driver1.findElements(By.xpath("//div[@class='table' and preceding-sibling::h4[contains(.,'Ки')]]//div[@class='contacts']/div[@class='phones']"));
